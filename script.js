15d8893da8f7eab582e984da25ef368672e9b382
akhilsukumaar <93520775+akhilsukumaar@users.noreply.github.com>
2023-11-25 21:39:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,23 @@ const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
+function showToast(message) {
+  const toastContainer = document.getElementById('toast-container');
+  toastContainer.textContent = message;
+  toastContainer.style.display = 'block';
+
+  // Hide the toast after 10 seconds
+  setTimeout(() => {
+    toastContainer.style.display = 'none';
+  }, 10000);
+}
+
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yay, see you on the 28th!";
   gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    "https://media.giphy.com/media/idFxmiV2dayJEqzXaW/giphy.gif";
+
+    showToast('Message sent to Akhil Sukumaran');
 });
 
 noBtn.addEventListener("mouseover", () => {
